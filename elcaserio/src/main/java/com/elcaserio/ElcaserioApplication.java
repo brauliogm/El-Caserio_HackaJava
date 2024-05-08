e3f1f8541cafcd0f5d1e204bc47e4a407d511e37
dickson1708 <129072304+dickson1708@users.noreply.github.com>
2024-05-08 15:40:29 +0200

    Add files via upload

---
+++ elcaserio/src/main/java/com/elcaserio/ElcaserioApplication.java
@@ -0,0 +1,13 @@
+package com.elcaserio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElcaserioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElcaserioApplication.class, args);
+	}
+
+}
