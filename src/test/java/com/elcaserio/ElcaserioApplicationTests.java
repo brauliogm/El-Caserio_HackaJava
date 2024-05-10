6530297ae9040e9c06b72e79c56f80bc6f9db3f7
dickson <dickson17081999@gmail.com>
2024-05-10 09:51:01 +0200

    Aplicando Spring al back

---
+++ src/test/java/com/elcaserio/ElcaserioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcaserio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElcaserioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
