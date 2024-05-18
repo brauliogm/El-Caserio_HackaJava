0077d62a3db73e0d6fe7e35f05cb94e227680b8e
brauliogm <brauliogm16@gmail.com>
2024-05-17 21:30:20 -0600

    Empezando con el formulario

--- FrontEnd/el-caserio/src/app/pedido/pedido.component.ts
+++ FrontEnd/el-caserio/src/app/pedido/pedido.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class PedidoComponent {
 
+  pedido: boolean = false;
+  datos: boolean = true;
+  resumen: boolean = false;
+  marcador: number = 1;
+
+  constructor(){
+
+  }
+
+  ngInit(){
+  }
+
+  ventanaDatos(){
+    this.datos = !this.datos;
+    this.pedido = !this.pedido;
+  }
+
+  ventanaResumen(){
+    this.resumen = !this.resumen;
+    this.datos = !this.datos;
+  }
+
+  sumarMarcador(){
+    this.marcador++;
+  }
+
+  restarMarcador(){
+    this.marcador--;
+  }
 }
