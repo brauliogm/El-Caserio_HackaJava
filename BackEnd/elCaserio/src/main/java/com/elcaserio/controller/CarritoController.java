09b98ab68c96ef8012885a98437f9589c30d752a
Jhonmex <financejhon@gmail.com>
2024-05-25 13:02:09 -0600

    Alergenos metodo buscar id

--- BackEnd/elCaserio/src/main/java/com/elcaserio/controller/CarritoController.java
+++ BackEnd/elCaserio/src/main/java/com/elcaserio/controller/CarritoController.java
@@ -14,8 +14,8 @@ public class CarritoController {
     ICarritoService iCarritoService;
 
     @GetMapping("/{id}")
-    public Carrito verCarrito(@PathVariable Long idCarrito) {
-        return iCarritoService.verCarrito(idCarrito);
+    public Carrito verCarrito(@PathVariable Long id) {
+        return iCarritoService.verCarrito(id);
     }
 
     @PostMapping("/crearCarrito")
@@ -25,8 +25,8 @@ public class CarritoController {
     }
 
     @DeleteMapping("/{id}")
-    public String vaciarCarrito(@PathVariable Long idCarrito) {
-        iCarritoService.vaciarCarrito(idCarrito);
+    public String vaciarCarrito(@PathVariable Long id) {
+        iCarritoService.vaciarCarrito(id);
         return "Carrito eliminado correctamente.";
     }
 
