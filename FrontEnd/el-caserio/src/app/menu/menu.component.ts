4a0fc82d2799d77d0de7f9708f32751b4c1adee4
brauliogm <brauliogm16@gmail.com>
2024-05-27 08:51:11 -0600

    Iteracion en el menu de las categorias

--- FrontEnd/el-caserio/src/app/menu/menu.component.ts
+++ FrontEnd/el-caserio/src/app/menu/menu.component.ts
@@ -14,11 +14,29 @@ export class MenuComponent {
   page: number = 1;
   pageSize: number = 6;
 
+  // Definir las opciones del grupo de radio
+  options = [
+    { id: '0', value: 'total', label: 'Todos' },
+    { id: '1', value: 'burgers', label: 'Burguers' },
+    { id: '2', value: 'entrantes', label: 'Entrantes' },
+    { id: '3', value: 'combinados especiales', label: 'Combinados Especiales' },
+    { id: '4', value: 'ensaladas', label: 'Ensaladas' },
+    { id: '5', value: 'sandwiches', label: 'Sandwiches' },
+    { id: '6', value: 'bocadillos', label: 'Bocadillos' },
+    { id: '7', value: 'perritos', label: 'Perritos' },
+    { id: '8', value: 'postres', label: 'Postres' },
+  ];
+
+  // Variable para almacenar la opción seleccionada
+  selectedOption: string = this.options[0].value;
+  resultado: string = '';
+
   constructor(private productoService: ProductoService,
             private enrutador: Router){}
 
   ngOnInit(){
     this.obtenerProductos();
+    
   }
 
   private obtenerProductos(){
@@ -27,6 +45,14 @@ export class MenuComponent {
         this.productos = datos;
       })
     );
+  }
+
+  filtrado(): number{
+    return this.productos.length;
+  }
+
+  cantidadCategoria(categoria: string){
+
   }
 
 }
